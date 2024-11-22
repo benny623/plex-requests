7c41244d38b4577d3563a2182e3cef290dabf6de
Ben Milner <bmilner88@gmail.com>
2024-11-21 16:42:39 -0800

    Add Deletion

--- components/admin-page.tsx
+++ components/admin-page.tsx
@@ -126,7 +126,6 @@ export default function AdminPage({ allRequests, loading }: adminProps) {
       }
 
       const updatedRequest = await response.json();
-
       console.log("Note updated", updatedRequest);
     } catch (err) {
       console.error("Error updating note:", err);
@@ -138,6 +137,36 @@ export default function AdminPage({ allRequests, loading }: adminProps) {
           : request
       );
       setRequests(revertedRequestList);
+    }
+  };
+
+  const handleDeletion = async (
+    e: React.MouseEvent<HTMLButtonElement>,
+    requestId: number
+  ) => {
+    e.preventDefault();
+    try {
+      // Send deletion to server
+      const response = await fetch(`/api/delete-request/${requestId}`, {
+        method: "DELETE",
+        headers: {
+          "Content-Type": "application/json",
+        },
+      });
+
+      if (!response.ok) {
+        throw new Error("Failed to delete note");
+      }
+
+      const deletedRequest = await response.json();
+      console.log("Request deleted", deletedRequest);
+
+      // Update local state by removing the deleted request
+      setRequests((prevRequests) =>
+        prevRequests.filter((request) => request.request_id !== requestId)
+      );
+    } catch (err) {
+      console.error("Error deleting note:", err);
     }
   };
 
@@ -167,6 +196,7 @@ export default function AdminPage({ allRequests, loading }: adminProps) {
           <table className="table w-full max-w-4xl border-collapse table-pin-rows">
             <thead>
               <tr>
+                <th></th>
                 <th>Title</th>
                 <th>Release Year</th>
                 <th>Type</th>
@@ -179,6 +209,27 @@ export default function AdminPage({ allRequests, loading }: adminProps) {
                 requests.length > 0 ? (
                   requests.map((request: Request) => (
                     <tr key={request.request_id}>
+                      <td>
+                        <button
+                          className="transition ease-in-out hover:scale-110 duration-200"
+                          onClick={(e) => handleDeletion(e, request.request_id)}
+                        >
+                          <svg
+                            xmlns="http://www.w3.org/2000/svg"
+                            fill="none"
+                            viewBox="0 0 24 24"
+                            strokeWidth={1.5}
+                            stroke="currentColor"
+                            className="size-6"
+                          >
+                            <path
+                              strokeLinecap="round"
+                              strokeLinejoin="round"
+                              d="m14.74 9-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 0 1-2.244 2.077H8.084a2.25 2.25 0 0 1-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 0 0-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 0 1 3.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 0 0-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 0 0-7.5 0"
+                            />
+                          </svg>
+                        </button>
+                      </td>
                       <td>{request.request_title}</td>
                       <td>{request.request_year}</td>
                       <td>{request.request_type}</td>
@@ -224,14 +275,14 @@ export default function AdminPage({ allRequests, loading }: adminProps) {
                   ))
                 ) : (
                   <tr>
-                    <td colSpan={5} style={{ textAlign: "center" }}>
+                    <td colSpan={6} style={{ textAlign: "center" }}>
                       No current requests
                     </td>
                   </tr>
                 )
               ) : (
                 <tr>
-                  <td colSpan={5} style={{ textAlign: "center" }}>
+                  <td colSpan={6} style={{ textAlign: "center" }}>
                     <span className="loading loading-dots loading-md"></span>
                   </td>
                 </tr>
