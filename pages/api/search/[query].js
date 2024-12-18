f3a4167ecffd4e351600595c101f4e8411407630
Ben Milner <bmilner88@gmail.com>
2024-12-17 17:27:09 -0800

    Updated media_type logic

--- pages/api/search/[query].js
+++ pages/api/search/[query].js
@@ -1,6 +1,27 @@
+const capitalizeFirst = (val) => {
+  return String(val).charAt(0).toUpperCase() + String(val).slice(1);
+};
+
 export default async function handler(req, res) {
-  const capitalizeFirst = (val) => {
-    return String(val).charAt(0).toUpperCase() + String(val).slice(1);
+  const getKeywords = async (type, id) => {
+    try {
+      const response = await fetch(
+        `${process.env.TMDB_BASE_URL}/${type}/${id}/keywords?api_key=${process.env.TMDB_API_KEY}`
+      );
+
+      if (!response.ok) {
+        throw new Error(`Error: ${response.statusText}`);
+      }
+
+      const data = await response.json();
+
+      // Send keyword data
+      return data.results || data.keywords;
+    } catch (err) {
+      res
+        .status(500)
+        .json({ error: "Internal Server Error", details: err.message });
+    }
   };
 
   try {
@@ -24,33 +45,58 @@ export default async function handler(req, res) {
         .json({ error: `Failed to fetch data: ${response.statusText}` });
     }
 
+    // Parse raw data to json
     const rawData = await response.json();
 
-    const data = rawData.results
-      ?.map((i) => {
+    const data = await Promise.all(
+      rawData.results?.map(async (i) => {
         if (i.media_type !== "person") {
+          // Await keyword data
+          const keywords = await getKeywords(i.media_type, i.id);
+
+          // Check if it's an Anime
+          const isAnime = keywords?.some((keyword) => keyword.id === 210024);
+
+          // Check if it's a Seasonal Movie
+          const isSeasonal = keywords?.some((keyword) => keyword.id === 65);
+
+          const getMediaType = (type) => {
+            if (!isAnime && !isSeasonal) {
+              if (type === "tv") {
+                return "TV Show";
+              }
+              return capitalizeFirst(type);
+            } else if (!isAnime && isSeasonal) {
+              if (type === "tv") {
+                return "TV Show";
+              }
+              return `Seasonal ${capitalizeFirst(type)}`;
+            } else if (isAnime && !isSeasonal) {
+              if (type === "tv") {
+                return "Anime";
+              }
+              return `Anime ${capitalizeFirst(type)}`;
+            }
+          };
+
           return {
             id: i.id,
+            keywords: keywords?.results || keywords?.keywords || [],
             title: i.title || i.name,
             year: `${i.release_date || i.first_air_date}`.split("-")[0],
             overview: i.overview,
             poster: i.poster_path,
-            media_type:
-              i.genre_ids?.[0] === 16 && i.original_language === "ja"
-                ? i.media_type === "movie"
-                  ? "Anime " + capitalizeFirst(i.media_type)
-                  : "Anime"
-                : i.media_type === "movie"
-                ? capitalizeFirst(i.media_type)
-                : "TV Show",
+            media_type: getMediaType(i.media_type),
             rating: Math.round(i.vote_average * 10) / 10,
           };
         }
         return null; // Filters out "person" items explicitly
       })
-      .filter(Boolean); // Removes null values
+    );
 
-    res.status(200).json(data);
+    const filteredData = data.filter(Boolean);
+
+    res.status(200).json(filteredData);
   } catch (err) {
     res
       .status(500)
