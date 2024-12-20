9c76c26b9ff3f33db4dc0d1b4f1419645b553139
Ben Milner <bmilner88@gmail.com>
2024-12-19 23:23:25 -0800

    Delete Confirmation Modal

--- components/deletion-modal.tsx
+++ components/deletion-modal.tsx
@@ -1,40 +1,52 @@
-// Modal
-{
-    /* <dialog id={`${request.request_id}_modal`} className="modal">
-                      <div className="modal-box">
-                        <form method="dialog">
-                          <button className="btn btn-sm btn-circle btn-ghost absolute right-2 top-2">
-                            ✕
-                          </button>
-                        </form>
-                        <h3 className="font-bold text-lg">
-                          Are you sure you want to delete this?
-                        </h3>
-                        <p className="my-4">Request: {request.request_title}</p>
-                        <div className="modal-action gap-4">
-                          <button
-                            className="btn btn-neutral"
-                            onClick={() => {
-                              (
-                                document.getElementById(
-                                  `${request.request_id}_modal`
-                                ) as HTMLDialogElement
-                              ).close();
-                            }}
-                          >
-                            Cancel
-                          </button>
-                          <button
-                            className="btn btn-error"
-                            onClick={(e) => handleDeletion(e, request.request_id)}
-                          >
-                            Delete
-                          </button>
-                        </div>
-                      </div>
-                      <form method="dialog" className="modal-backdrop">
-                        <button>close</button>
-                      </form>
-                    </dialog> */
-  }
-  
+type DeletionModalProps = {
+  isOpen: boolean;
+  setIsOpen: React.Dispatch<React.SetStateAction<boolean>>;
+  //title: string;
+};
+
+const DeletionModal: React.FC<DeletionModalProps> = ({ isOpen, setIsOpen }) => {
+  const handleCancel = () => {
+    setIsOpen(false);
+  };
+
+  const handleConfirm = (e: React.MouseEvent<HTMLButtonElement>) => {
+    onDelete(e, request.request_id);
+    setIsOpen(false);
+  };
+
+  return (
+    <>
+      {isOpen && (
+        <dialog open className="modal">
+          <div className="modal-box">
+            <form method="dialog">
+              <button
+                className="btn btn-sm btn-circle btn-ghost absolute right-2 top-2"
+                onClick={handleCancel}
+              >
+                ✕
+              </button>
+            </form>
+            <h3 className="font-bold text-lg">
+              Are you sure you want to delete this?
+            </h3>
+            <p className="my-4">Request: {request.request_title}</p>
+            <div className="modal-action gap-4">
+              <button className="btn btn-neutral" onClick={handleCancel}>
+                Cancel
+              </button>
+              <button className="btn btn-error" onClick={handleConfirm}>
+                Delete
+              </button>
+            </div>
+          </div>
+          <form method="dialog" className="modal-backdrop">
+            <button onClick={handleCancel}>close</button>
+          </form>
+        </dialog>
+      )}
+    </>
+  );
+};
+
+export default DeletionModal;
